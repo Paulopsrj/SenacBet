ec58eb7b4819e3337d284ac00243d012542f1820
Paulopsrj <paulopsrj92@gmail.com>
2022-09-28 21:56:56 -0300

    att senacbet2

--- src/main/java/com/senac/SenacBet/controller/ContatoController.java
+++ src/main/java/com/senac/SenacBet/controller/ContatoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.model.IModel;
 
@@ -31,6 +32,28 @@ public class ContatoController {
         model.addAttribute("contatos", dao.findAll());
         return "lista-contato";
     }
+
+    @GetMapping("/contato/delete/{id}")
+    public String apagarContato(@PathVariable("id") Integer id, Model model){
+        dao.deleteById(id);
+        return "redirect:/listar-contatos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editContato(@PathVariable("id") Integer id, Model model) {
+        Contato contato = dao.findById(id).get();
+        model.addAttribute("contato" , contato);
+        return "edit-contato";
+    }
+
+    @PostMapping("/edit-contato/{id}")
+    public String atualizarContato(@PathVariable Integer id , Contato contato){
+        dao.save(contato);
+        return "redirect:/listar-contatos";
+    }
+
+
+
 
 
 }
--- src/main/java/com/senac/SenacBet/controller/PerfilController.java
+++ src/main/java/com/senac/SenacBet/controller/PerfilController.java
@@ -1,11 +1,13 @@
 package com.senac.SenacBet.controller;
 
 import com.senac.SenacBet.dao.PerfilDAO;
+import com.senac.SenacBet.model.Contato;
 import com.senac.SenacBet.model.Perfil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,4 +33,21 @@ public class PerfilController {
         return "lista-perfil";
     }
 
+    @GetMapping("/perfil/delete/{perfil}")
+    public String deletePerfil(@PathVariable("perfil") String cpf) {
+        dao.deleteById(cpf);
+        return "redirect:/listar-perfil";
+    }
+    @GetMapping("/edit/perfil/{cpf}")
+    public String editPerfil(@PathVariable("cpf") String cpf, Model model) {
+        Perfil perfil = dao.findById(cpf).get();
+        model.addAttribute("perfil" , perfil);
+        return "edit-perfil";
+    }
+
+    @PostMapping("/edit-perfil/{cpf}")
+    public String atualizarPerfil(@PathVariable String cpf, Perfil perfil) {
+        dao.save(perfil);
+        return "redirect:/listar-perfil";
+    }
 }
